51e35d7fde6d5e80c2242f0d4358a7acebb53d61
Skythethird <59905943+Skythethird@users.noreply.github.com>
2021-05-25 00:45:29 +0700

    add image

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,5 +1,9 @@
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sokoV3/test.dart';
 
 class Detailpage extends StatefulWidget {
   @override
@@ -7,31 +11,169 @@ class Detailpage extends StatefulWidget {
 }
 
 class _DetailpageState extends State<Detailpage> {
-  int _counter = 0;
+ TextEditingController _controller = TextEditingController();
+  File _image;
 
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
+  var ImagePicker;
+  var ImageSource;
   @override
-  Widget build(BuildContext context) {
+  _imgFromCamera() async {
+  File image = await ImagePicker.pickImage(
+    source: ImageSource.camera, imageQuality: 50
+  );
+
+  setState(() {
+    _image = image;
+  });
+  }
+
+  _imgFromGallery() async {
+  File image = await  ImagePicker.pickImage(
+      source: ImageSource.gallery, imageQuality: 50
+  );
+
+  setState(() {
+    _image = image;
+  });
+  }
+  void _showPicker(context) {
+  showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return SafeArea(
+          child: Container(
+            child: new Wrap(
+              children: <Widget>[
+                new ListTile(
+                    leading: new Icon(Icons.photo_library),
+                    title: new Text('Photo Library'),
+                    onTap: () {
+                      _imgFromGallery();
+                      Navigator.of(context).pop();
+                    }),
+                new ListTile(
+                  leading: new Icon(Icons.photo_camera),
+                  title: new Text('Camera'),
+                  onTap: () {
+                    _imgFromCamera();
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    );
+}
+
+  @override
+  void initState(){
+        _controller.text = "0"; 
+    super.initState();
+  }
+
+  @override
+    Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('test'),
+      appBar: AppBar(
+        title: Text('Number Field increment decrement'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              onChanged: (text){
-              }
-            )
-          ],
-        ), 
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Container(
+            width: 60.0,
+            foregroundDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5.0),
+              border: Border.all(
+                color: Colors.blueGrey,
+                width: 2.0,
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: TextFormField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(8.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    controller: _controller,
+                    keyboardType: TextInputType.numberWithOptions(
+                      decimal: false,
+                      signed: true,
+                    ),
+                    inputFormatters: <TextInputFormatter>[
+                      WhitelistingTextInputFormatter.digitsOnly
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 38.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              width: 0.5,
+                            ),
+                          ),
+                        ),
+                        child: InkWell(
+                          child: Icon(
+                            Icons.add,
+                            size: 18.0,
+                          ),
+                          onTap: () {
+                            int currentValue = int.parse(_controller.text);
+                            setState(() {
+                              currentValue++;
+                              _controller.text = (currentValue)
+                                  .toString(); // incrementing value
+                            });
+                          },
+                        ),
+                      ),
+                      InkWell(
+                        child: Icon(
+                          Icons.remove,
+                          size: 18.0,
+                        ),
+                        onTap: () {
+                          int currentValue = int.parse(_controller.text);
+                          setState(() {
+                            print("Setting state");
+                            currentValue--;
+                            _controller.text =
+                                (currentValue > 0 ? currentValue : 0)
+                                    .toString(); // decrementing value
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      
+      floatingActionButton: FloatingActionButton(
+        onPressed:(){ Navigator.push(
+          context, 
+          MaterialPageRoute(builder: (context)=> Pic()));
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
