14c41771be32ac671a97d22dd8149707ceb2b7a7
flukgaro <pitpagon@hotmail.com>
2021-05-28 14:21:41 +0700

    55

--- lib/Widget/list_item_widget.dart
+++ lib/Widget/list_item_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:focused_menu/focused_menu.dart';
 import 'package:focused_menu/modals.dart';
 import 'package:sokoV3/model/list_item.dart';
-
+import 'package:sokoV3/Searchbar.dart';
 import '../detail_item.dart';
 
 class ListItemWidget extends StatelessWidget {
@@ -42,37 +42,83 @@ class ListItemWidget extends StatelessWidget {
             item.title,
             style: TextStyle(fontSize: 20, color: Colors.black),
           ),
-          trailing: FocusedMenuHolder(
-            menuItems: [
-                        FocusedMenuItem(
-              title: Text('แก้ไข'),
-              trailingIcon: Icon(Icons.edit_sharp),
-              // onPressed:
-                        ),
-                        FocusedMenuItem(
-              title: Text('ลบ'),
-              trailingIcon: Icon(
-                Icons.delete_forever,
-              ),
-              backgroundColor: Colors.red,
-              // onPressed:
-                        ),
-                      ],
-                      blurBackgroundColor: Colors.blueGrey[900],
-                      openWithTap: true,
-                      onPressed: () {},
-                        child: IconButton(
-                icon: Icon(
-                  Icons.more_horiz,
-                  color: Colors.black,
-                  size: 32,
-                ),
-                onPressed: onClicked,
-              ),
-          ),
+          trailing: _simplePopup(context),
+          // trailing: IconButton(
+          //   onPressed: _simplePopup,
+                
+          //       icon: Icon(
+          //         Icons.more_horiz,
+          //         color: Colors.black,
+          //         size: 32,
+          //       ),
+                
+                
+          //     ),
         ),
       ),
   );
+
+
+Widget _simplePopup(BuildContext context) => Container(
+  width: 40,height: 40,
+  decoration: BoxDecoration(borderRadius: BorderRadius.circular(64),color: Colors.black),
+  child:   PopupMenuButton<int>(
+  
+            child: Icon(Icons.more_vert,color: Colors.white),
+            onSelected:(int value) {
+              
+              print(value);
+              if(value == 1){
+              
+              showDialog(context: context,builder: (_) => AlertDialog(title: Text("เพิ่ม/ลด สินค้า"),));
+              }else if(value == 2){
+                //  Navigator.of(context).pushNamed(route)
+              // Navigator.of(context).push(MaterialPageRoute(builder: (context) => SecondRoute()),)
+              }else if(value == 3){
+                showDialog(context: context,builder: (_) => AlertDialog(
+                  title: Text("ลบสินค้า"),
+                  content: Text("ต้องการลบสินค้านี้หรือไม่ ?"),
+                  actions: [
+                    TextButton(child:Text("ใช่! ฉันต้องการลบ"),onPressed: (){},),
+                    TextButton(child:Text("ยกเลิก"),onPressed: (){},),
+                    ],
+                    elevation: 24.0,
+                ));
+              }
+            },
+  
+            itemBuilder: (context) => [
+  
+                  PopupMenuItem(
+  
+                    value: 1,
+  
+                    child: Text("เพิ่ม/ลด สินค้า"),
+  
+                  ),
+  
+                  PopupMenuItem(
+  
+                    value: 2,
+  
+                    child: Text("แก้ไข"),
+  
+                  ),
+  
+                  PopupMenuItem(
+  
+                    value: 3,
+  
+                    child: Text("ลบ"),
+                    
+  
+                  ),
+  
+                ],
+  
+          ),
+);
+
 
 //   Widget buildItem() => Container(
 //     width: double.infinity,
@@ -116,4 +162,8 @@ class ListItemWidget extends StatelessWidget {
 //       )
 //     : CircularProgressIndicator(),
 //   );
+
+
+
+
 }
